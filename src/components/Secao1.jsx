d6d209fa41c8fcc87fe7e5ba25134cd24679722b
antonio BC Energia <antonio.carlos@grupobcenergia.com.br>
2024-05-01 20:23:58 -0300

    first commit]

--- src/components/Secao1.jsx
+++ src/components/Secao1.jsx
@@ -66,9 +66,108 @@ function Sobre() {
               • Frituras • Enlatados • Salsichas • Refrigerantes • Café •
               Salgadinhos • Balas • Açúcar adicionado nos alimentos
             </p>
-            <p className="font-sobre mb-3" data-aos="zoom-in"></p>
-            <p className="font-sobre mb-3" data-aos="zoom-in"></p> O Que Evitar:
           </div>
+        </div>
+      </div>
+      <h2 className="subtitulo" data-aos="fade-right">
+        Importância da Alimentação na Primeira Infância
+      </h2>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="sobre-container">
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            A primeira infância é a idade em que os pais apresentam às crianças
+            novos alimentos e sabores. Esse é o momento ideal para associar o
+            gosto da comida saudável ao paladar dos pequenos, favorecendo assim
+            a saúde e o desenvolvimento. Pode ser definida como o período desde
+            o nascimento da criança até os 8 anos de idade.Estamos perante um
+            período crucial para o pequeno no que diz respeito ao seu
+            desenvolvimento, pois é nesta fase que se criam as bases necessárias
+            à aprendizagem e ao bem-estar que o acompanharão para o resto da
+            vida.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            Alguns dos problemas de riscos com uma alimentação inadequada:
+            Obesidade A obesidade infantil acomete crianças até 12 anos e é
+            caracterizada pelo excesso de peso para a idade. O diagnóstico pode
+            ser feito por meio do cálculo do Índice de Massa Corporal (IMC), que
+            considera idade, altura e peso. Crianças com obesidade têm 80% de
+            chances de se tornar um adulto obeso, segundo o Ministério da Saúde.
+            Além disso, a obesidade infantil está relacionada a outras 26
+            doenças crônicas que deixaram de ser exclusivas de adultos.
+            Atualmente, cada vez mais crianças já têm problemas de diabetes tipo
+            2, pressão alta e colesterol elevado. Coração, rins e cérebro também
+            podem ser afetados por esse mal.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            {" "}
+            A alimentação, de fato, é um dos primeiros fatores que vem à cabeça,
+            por isso, vamos começar por ela. Não há dúvidas de que cardápios
+            ricos em comidas industrializadas, gordurosas e repletas de açúcar
+            não são nada saudáveis. Por vezes, a alimentação inadequada já é
+            algo comum dentro da família e, obviamente, reflete nos pequenos. Os
+            lanchinhos, por exemplo, são refeições que merecem atenção, porque
+            são justos neles que comidas nada saudáveis são inseridas. Diabetes
+            Diabetes também é uma das doenças crônicas mais comuns de serem
+            encontradas no público infantil, sendo que cerca de 20 a cada 100.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            crianças e adolescentes chegam a desenvolver a doença a cada ano,
+            geralmente o Diabetes Mellitus tipo 1 ou DM1. Como o diabetes
+            infantil influencia o organismo em um organismo normal, o açúcar
+            ingerido na alimentação através de comidas ricas em carboidratos,
+            como arroz, massas, doces e frutas, é convertido em glicose no
+            intestino. O pâncreas, órgão responsável pela produção do hormônio
+            insulina, por sua vez transporta a glicose do sangue para dentro das
+            células. No entanto, em crianças com DM1, o órgão deixa de produzir
+            insulina, e consequentemente a glicose não consegue chegar até às
+            células, acarretando uma alta taxa de açúcar no sangue.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            Não são claros os motivos pelos quais as crianças deixam de produzir
+            insulina, porém sabe-se que há uma associação entre tendência
+            hereditária e alguma influência ambiental, como uma infecção por
+            vírus, por exemplo. Neste caso, o corpo tenta eliminar o vírus,
+            porém passa a destruir suas próprias células produtoras de insulina,
+            resultando assim em uma doença autoimune. Alguns sintomas da doença
+            em crianças e adolescentes ao longo do tempo podem ser indícios de
+            diabetes infantil, como por exemplo:<br></br>• – Sentir muita fome,
+            comer muito, porém emagrecer ou não ganhar peso; • – Ter muita sede;
+            • – Urinar muito e com frequência; • – Apresentar visão embaçada; •
+            – Sentir fraqueza ou falta de disposição para praticar atividades
+            diárias.
+          </p>
+        </div>
+      </div>
+      <h2 className="subtitulo" data-aos="fade-right">
+        Desnutrição
+      </h2>
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="sobre-container">
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            A desnutrição infantil é uma condição clínica decorrente de uma
+            deficiência ou excesso, relativo ou absoluto, de um ou mais
+            nutrientes essenciais, segundo definição do Ministério da Saúde. Ela
+            pode ser causada por ingestão insuficiente de alimentos ou por má
+            alimentação, ou seja, consumir produtos não saudáveis. Falta de
+            nutrientes na primeira infância compromete o desenvolvimento físico
+            e psíquico. E está associada não apenas à escassez de comida, mas
+            também à qualidade do que chega ao prato das crianças. O principal
+            sintoma da desnutrição é a dificuldade para ganhar peso.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in">
+            Quando o peso não aumenta ou regride na curva de crescimento, a
+            criança pode estar desnutrida. Esse sintoma pode ser acompanhado por
+            falta de força e energia, além de dificuldade de concentração e
+            crescimento atrofiado. Em casos graves, podem ocorrer inchaços no
+            estômago, face e pernas, além da mudança na pigmentação da pele. A
+            fase de maior atenção é a que vai até os 2 anos. Bebês desnutridos
+            têm o sistema imunológico enfraquecido e são menos resistentes às
+            doenças comuns da infância. Resfriados e diarreias, por exemplo,
+            podem ser fatais.
+          </p>
+          <p className="font-sobre mb-3" data-aos="zoom-in"></p>
+          <p className="font-sobre mb-3" data-aos="zoom-in"></p>
+          <p className="font-sobre mb-3" data-aos="zoom-in"></p>
         </div>
       </div>
     </div>
